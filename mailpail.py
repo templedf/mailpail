0200e5168b35e2d914e5eb2c9c40f7696c26f16b
Daniel Templeton <templedf@gmail.com>
2022-10-10 19:17:09 -0700

    Added license header

--- mailpail.py
+++ mailpail.py
@@ -6,6 +6,20 @@ Created on Mon Oct 10 09:14:39 2022
 @author: dtempleton
 """
 
+ # Copyright 2022 Daniel Templeton
+
+ #   Licensed under the Apache License, Version 2.0 (the "License");
+ #   you may not use this file except in compliance with the License.
+ #   You may obtain a copy of the License at
+
+ #       http://www.apache.org/licenses/LICENSE-2.0
+
+ #   Unless required by applicable law or agreed to in writing, software
+ #   distributed under the License is distributed on an "AS IS" BASIS,
+ #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ #   See the License for the specific language governing permissions and
+ #   limitations under the License.
+
 # Script to deduplicate personal information by address to create
 # customized mailing labels.  Also joins customer data with
 # historical data for voting counts.
